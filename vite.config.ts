d1715558cdbd5cd7788363cf8bd5ccf5526a5323
JalukOne <iblezya@hotmail.com>
2022-11-04 01:42:52 -0500

    multi-pages add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        projects: resolve(__dirname, 'pages/projects/index.html')
+      }
+    },
     outDir: '../dist'
   }
-}
+})
